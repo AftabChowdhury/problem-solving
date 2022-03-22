e2b5b406c06c53b247a4d66086efc4640e2b3ca0
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-03-23 00:53:21 +0100

    Validate_Subsequence: add solution 2

--- Validate_Subsequence.py
+++ Validate_Subsequence.py
@@ -17,4 +17,17 @@ def is_valid_subsequence_solution1(array, sequence):
     return sequence_index == len(sequence)
 
 
+# O(n) time | O(1) space - where n is the length of the array
+def is_valid_subsequence_solution2(array, sequence):
+    # Write your code here.
+    sequence_index = 0
+    for value in array:
+        if sequence_index == len(sequence):
+            break
+        if value == sequence[sequence_index]:
+            sequence_index += 1
+    return sequence_index == len(sequence)
+
+
 print(is_valid_subsequence_solution1([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]))
+print(is_valid_subsequence_solution2([5, 1, 22, 25, 6, -1, 8, 10], [1, 1, 6, -1, 10]))
