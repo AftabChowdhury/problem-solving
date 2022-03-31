d45c9a07504db2b63d94da6332702c12975e4b1b
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-03-31 22:42:32 +0200

    Find_Closest_Value_BST: implement the solution Find_Closest_Value_BST with binary search tree

---
+++ Find_Closest_Value_BST.py
@@ -0,0 +1,23 @@
+def find_closest_value_In_bst(tree, target):
+    return get_closest_value(tree, target, tree.value)
+
+
+def get_closest_value(tree, target, closest_value):
+    current_tree = tree
+    while current_tree is not None:
+        if abs(target - current_tree.value) < abs(target - closest_value):
+            closest_value = current_tree.value
+        if target < current_tree.value:
+            current_tree = current_tree.left
+        elif target > current_tree.value:
+            current_tree = current_tree.right
+        else:
+            break
+    return closest_value
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
