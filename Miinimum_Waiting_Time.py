b64f65675aac261cb0462a5bca36a7873812ae05
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-04-04 18:16:31 +0200

    Miinimum_Waiting_Time: add problem description

--- Miinimum_Waiting_Time.py
+++ Miinimum_Waiting_Time.py
@@ -1,3 +1,17 @@
+"""
+
+  You're given a non-empty array of positive integers representing the amounts
+  of time that specific queries take to execute. Only one query can be executed
+  at a time, but the queries can be executed in any order.
+
+  A query's waiting time is defined as the amount of time that it must
+  wait before its execution starts. In other words, if a query is executed
+  second, then its waiting time is the duration of the first query; if a query
+  is executed third, then its waiting time is the sum of the durations of the
+  first two queries.
+"""
+
+
 def minimum_waiting_time(queries):
     queries.sort()
     sum = 0
