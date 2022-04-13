c2bc25209b0f49cfc1003db68451c699c318a642
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-04-14 00:20:08 +0200

    Caesar_Cipher_Encryptor: implement Caesar_Cipher_Encryptor

---
+++ Caesar_Cipher_Encryptor.py
@@ -0,0 +1,20 @@
+def caesar_cipher_encryptor(string, key):
+    # Write your code here.
+    encrypted_array = []
+    for i in string:
+        cipher_value = get_cipher_index(i, key)
+        encrypted_array.append(cipher_value)
+    return ''.join(encrypted_array)
+
+
+def get_cipher_index(character, key):
+    all_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    cipher_index = all_alphabet.index(character) + key
+    if cipher_index > 25:
+        cipher_index = cipher_index % 26
+    return all_alphabet[cipher_index]
+
+
+print(caesar_cipher_encryptor('xyz', 2))
+
+
