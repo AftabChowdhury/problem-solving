ae1035bcb3373f912c5f29906c9e606846964617
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-04-20 23:23:19 +0200

    Binary_Search: refactor the logic

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,9 +1,8 @@
 def binarySearch(array, target):
     left = 0
     right = len(array) - 1
-    mid = (left + right) / 2
-    mid = int(mid)
     while left <= right:
+        mid = int((left + right) / 2)
         if target == array[mid]:
             return mid
         elif target < array[mid]:
